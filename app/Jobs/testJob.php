d053be9e3b8b5916638b651aa7e195529a222ed5
David Valbuena <davidvalbuena99@gmail.com>
2021-06-21 08:06:55 -0500

    multitenant with cerberus

---
+++ app/Jobs/testJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class testJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        logger()->info('...........LOG FROM JOB............');
+        logger()->info('...........CREATING USER............');
+        $user = User::factory()->create();
+        logger()->info('USER WAS CREATED: ', [
+                'USER' => $user->toArray(),
+            ]);
+        logger()->info('...........ENDING JOB............');
+    }
+}
